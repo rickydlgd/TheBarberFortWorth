25281ea26dbd82c6eb8fed39bfa9d8c7bf94f541
rickydlgd <61253965+rickydlgd@users.noreply.github.com>
2021-01-07 23:59:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function myNavFunc(){
     if( (navigator.platform.indexOf("iPhone") != -1)
         || (navigator.platform.indexOf("iPod") != -1)
         || (navigator.platform.indexOf("iPad") != -1))
-         window.open("maps://goo.gl/maps/AYzoog3w88NFtM5cA");
+         window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[32.7071641],[-97.3334757]");
     else
-         window.open("https://goo.gl/maps/AYzoog3w88NFtM5cA");
+         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[32.7071641],[-97.3334757]");
 }
